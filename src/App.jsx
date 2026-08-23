adcde070ee688c3331a2f9bc607f5c9718110424
Kedija <kedijanejmudin.1@gmail.com>
2026-08-23 14:15:06 -0700

    Add Banner component with TMDB fetch, styling, and required utility files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "./Components/Banner/Banner";
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
