5c945a73203a46b39a4552e77901aa3089d208ff
Ashe-FSWD <ashenafieliascs@gmail.com>
2026-08-24 18:20:22 +0300

    App.jsx added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Banner />
+      <DisplayRow />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
